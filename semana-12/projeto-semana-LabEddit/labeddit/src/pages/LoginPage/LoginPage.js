9ecc9f2b3d8a6619292b71e120f401b768b843a7
Eduardo Henrique Sousa Moraes <dudugugusm@gmail.com>
2021-10-27 22:59:45 -0300

    página de login

--- semana-12/projeto-semana-LabEddit/labeddit/src/pages/LoginPage/LoginPage.js
+++ semana-12/projeto-semana-LabEddit/labeddit/src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,65 @@
 import React from "react";
-
+import { ConteinerDaTela, ConteinerDoBotao, ConteinerInputs, LogoImagem } from "./styled";
+import redeSocial2 from "../../assets/redeSocial2.jpg"
+import { Button, TextField } from "@material-ui/core";
+import useForm from "../../hooks/useForm";
+import { useHistory } from "react-router";
+import { irParaCadastro } from "../../routes/coordinator";
 const LoginPage = () => {
+    const [form, onChange, clear] = useForm({email:"", password: ""})
+    const onSubmitForm = (event) => {
+       event.preventDefault()
+    }
+    const history = useHistory()
     return(
-        <div>
-            LoginPage
-        </div>
+        <ConteinerDaTela>
+            <LogoImagem src={redeSocial2}/>
+            <ConteinerInputs>
+                <form onSubmit={onSubmitForm}>
+                    <TextField 
+                    type={"email"}
+                    name={"email"}
+                    value={form.email}
+                    onChange={onChange}
+                    label={"E-mail"}
+                    variant={"outlined"}
+                    fullWidth
+                    margin={"normal"}
+                    required
+                    />
+                    <TextField 
+                    type={"password"}
+                    name={"password"}
+                    value={form.password}
+                    onChange={onChange}
+                    label={"Senha"}
+                    variant={"outlined"}
+                    fullWidth
+                    margin={"normal"}
+                    required
+                    />
+                    <Button
+                    type={"submit"}
+                    fullWidth
+                    variant={"contained"}
+                    color={"primary"}
+                    >
+                    Fazer Login!   
+                    </Button>
+                </form>
+            </ConteinerInputs>
+            <ConteinerDoBotao>
+                <Button
+                type={"submit"}
+                fullWidth
+                color={"primary"}
+                variant={"outlined"}
+                onClick={()=> irParaCadastro(history)}
+                >
+                Não possue conta? Cadastre-se
+                </Button>
+            </ConteinerDoBotao>
+        </ConteinerDaTela>
     )
 }
 export default LoginPage
